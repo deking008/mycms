6b8e2507778641aa4d8d0e2be1c2db270bac41ab
deking008 <519820597@qq.com>
2017-07-07 00:33:48 +0800

    editdb

--- src/common/config/db.js
+++ src/common/config/db.js
@@ -9,7 +9,7 @@ export default {
     mysql: {
       host: '127.0.0.1',
       port: '3306',
-      database: 'cmswing',
+      database: 'cms',
       user: 'root',
       password: '123456',
       prefix: 'cmswing_',
