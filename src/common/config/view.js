23ba04140d547d7d5640eecce7a6565c2d31dce9
deking008 <519820597@qq.com>
2017-07-11 18:06:28 +0800

    fefwfwf

--- src/common/config/view.js
+++ src/common/config/view.js
@@ -10,10 +10,8 @@ export default {
     root_path: think.ROOT_PATH + '/view',
     adapter: {
         nunjucks: {
-
-        	prerender: (nunjucks, env) => {
-
-                 /**
+            prerender: (nunjucks, env) => {
+            	/**
                  * 获取用户组
                  */
                 env.addFilter('get_member_group',async (groupid,callback)=>{
@@ -21,9 +19,29 @@ export default {
                     callback(null,data[0]);
                 },true);
 
-        	}
-        	
+                /**
+                 * moment
+                 * YYYY-MM-DD HH:mm:ss
+                 * lll
+                 */
+                env.addFilter("moment",function (time,config) {
+                    let moment = require('moment');
+                    moment.locale('zh-cn');
+                    if(think.isEmpty(config)){
+                        return moment(time).fromNow();
+                    }else {
+                        return moment(time).format(config);
+                    }
+                })
 
+                /**
+                 * 数字转ip
+                 */
+                env.addFilter("int2ip",function (int) {
+                    return _int2iP(int);
+                })
+
+            }
         }
     }
 };
